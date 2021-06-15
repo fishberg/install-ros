953d7b558df2a89146c92ed063b30ce9ceffaa45
Andrew Fishberg <fishberg.dev@gmail.com>
2021-06-15 16:49:49 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Install ROS
+Simple script to install ROS. Useful for systems without a GUI where you can't easily copy/paste the commands directly from the ROS website.
---
+++ setup.sh
@@ -0,0 +1,31 @@
+# http://wiki.ros.org/noetic/Installation/Ubuntu
+
+# 1.1 Configure your Ubuntu respositories
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo apt update
+
+# Modification
+sudo apt install curl
+
+# 1.2 Setup your sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# 1.3 Setup your keys
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+# 1.4 Installation
+sudo apt update
+sudo apt install ros-noetic-ros-base # no desktop environment
+
+# 1.5 Environment setup
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# 1.6 Dependencies for building packages
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+# 1.6.1 Initialize rosdep
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
